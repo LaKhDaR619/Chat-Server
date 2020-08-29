cea52cb972f4f463a2fdfbb0b64bcd11640773ac
Lakhdar Ben Boudina <lakhdar.boudina@gmail.com>
2020-08-29 19:29:36 +0100

    some changes

---
+++ client/build/precache-manifest.fffc09e6ce57d267cab8a98c28a8cd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd90a28df32a869c332723b6857c953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69c6c5268031292cc7c",
+    "url": "/static/css/main.5a758b62.chunk.css"
+  },
+  {
+    "revision": "ad813333fb436246ffff",
+    "url": "/static/js/2.79c56887.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.79c56887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69c6c5268031292cc7c",
+    "url": "/static/js/main.85a5f402.chunk.js"
+  },
+  {
+    "revision": "925240659d9cec2563b2",
+    "url": "/static/js/runtime-main.7861498c.js"
+  },
+  {
+    "revision": "ac905963f5794835331c32a4908c6c2f",
+    "url": "/static/media/stairs.ac905963.mp3"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad66827958088fecc7fec39d878494c9.js"
+  "/precache-manifest.fffc09e6ce57d267cab8a98c28a8cd43.js"
 );
 
 self.addEventListener('message', (event) => {
